1dc5abf0f02b5cfde916d84e795eb1e63b8463ad
Dongryul KIM <doctor.k1994@gmail.com>
2023-02-02 04:05:55 +0900

    [feat] 상품후기 공지 아코디언 적용  #15

---
+++ client/js/productDetail/productDetail_dr.js
@@ -0,0 +1,16 @@
+
+
+const coll = document.getElementsByClassName("collapsible");
+let i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    const folded = this.parentElement.nextElementSibling;
+    if (folded.style.display === "block") {
+      folded.style.display = "none";
+    } else {
+      folded.style.display = "block";
+    }
+  });
+}
