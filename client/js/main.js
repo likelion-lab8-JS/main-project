e0e2ab52acb354010093a38e991826323e211946
Juhee Noh <nohjuhee227@gmail.com>
2023-02-01 18:09:50 +0900

    [feat] main_banner swiper 기능 추가 #18

--- client/js/main.js
+++ client/js/main.js
@@ -1,7 +1,26 @@
 import { getNode } from './main_page/index.js'
 
+/* -------------------------------------------------------------------------- */
+/*                                popup_beauty                                */
+/* -------------------------------------------------------------------------- */
 window.onload = function () {
   getNode('.close_popup').onclick = function () {
       getNode('#modal').style.display = "none"
   };
 };
+
+/* -------------------------------------------------------------------------- */
+/*                                 main_banner                                */
+/* -------------------------------------------------------------------------- */
+const swiper = new Swiper('.swiper_main_banner', {
+  autoplay: true,
+  autoplay: {
+    disableOnInteraction: false,
+  }, 
+  loop: true,
+  speed: 1000,
+  navigation: {
+    nextEl: '.main_banner_next',
+    prevEl: '.main_banner_prev',
+  },
+})
