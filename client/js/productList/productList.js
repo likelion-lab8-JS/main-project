e08716d4bf33bc1a9bebaf6e602208280efe315f
hayoenn2 <dwd1205@naver.com>
2023-02-02 10:52:41 +0900

    [feat] navigator 체크박스 활성화 및 기능 리팩토링

--- client/js/productList/productList.js
+++ client/js/productList/productList.js
@@ -1,13 +1,27 @@
 import { getNodes } from "../../lib/dom/getNode.js";
 
 const categoryTitle = getNodes(".top_menu");
-
 const categoryList = getNodes(".list_wrapper");
-const toggleBtn = getNodes(".toggle_btn > svg");
+const img = getNodes(".toggle_btn > svg");
+const list = getNodes(".category_list");
+const checked = getNodes(".check_btn");
+
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener("click", function (e) {
+    e.preventDefault();
+    let iconBtn = e.target.closest(".category_list > a");
+    if (!iconBtn) return;
+
+    checked[i].classList.toggle("none_btn");
+  });
+}
 
 for (let i = 0; i < categoryTitle.length; i++) {
-  categoryTitle[i].addEventListener("click", function () {
-    toggleBtn[i].classList.toggle("active");
+  categoryTitle[i].addEventListener("click", function (e) {
+    let toggleBtn = e.target.closest(".toggle_btn");
+    if (!toggleBtn) return;
+
+    img[i].classList.toggle("active");
 
     if (categoryList[i].style.maxHeight) {
       categoryList[i].style.maxHeight = null;
