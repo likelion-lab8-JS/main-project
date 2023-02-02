b89061a7a773639d3f43116eecb3e548abd03316
Juhee Noh <nohjuhee227@gmail.com>
2023-02-02 15:21:20 +0900

    [feat] 오늘의 추천 상품 처음과 끝 슬라이드에서 버튼 사라지는 기능 추가 #21

--- client/js/main.js
+++ client/js/main.js
@@ -28,6 +28,9 @@ const mainBannerSwiper = new Swiper('.swiper_main_banner', {
 /* -------------------------------------------------------------------------- */
 /*                                  오늘의 상품 추천                                 */
 /* -------------------------------------------------------------------------- */
+const recommend_products_prev = getNode('.recommend_products_prev');
+const recommend_products_next = getNode('.recommend_products_next');
+
 const todayRecommendSwiper = new Swiper('.swiper_recommend_products', {
   slidesPerView: 4,
   slidesPerGroup : 4,
@@ -37,4 +40,28 @@ const todayRecommendSwiper = new Swiper('.swiper_recommend_products', {
     nextEl: '.recommend_products_next',
     prevEl: '.recommend_products_prev',
   },
+  on: {
+    slideChange: function (){
+      // 여기서 this가 뭘까..?
+      // 함수 내에서 this는 해당 함수를 호출하기 위해 사용된 객체를 의미하는데,,
+      // 그럼 on 객체인데, this 대신 on을 넣으면 제대로 기능이 동작하지 않는다..ㅜㅜ
+      // ReferenceError: on is not defined
+
+      // console.log(this);
+      // V라고 정의된 object가 나옴... 어디서 튀어나온거지..
+
+      if (this.activeIndex === 4) {
+        recommend_products_prev.style.display = 'flex';
+        recommend_products_next.style.display = 'flex';
+      }
+      if (this.realIndex === 8) {
+        recommend_products_prev.style.display = 'flex';
+        recommend_products_next.style.display = 'none';
+      }
+      if (this.realIndex === 0) {
+        recommend_products_prev.style.display = 'none';
+        recommend_products_next.style.display = 'flex';
+      }
+      // activeIndex와 realIndex의 차이..?
+  }}
 })
