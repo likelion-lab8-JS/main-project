202eecb83bf0f6eee3248af55d0199418131cab8
hayoenn2 <dwd1205@naver.com>
2023-02-02 15:46:52 +0900

    [feat] 상품 리스트 페이지 navigator 버튼활성화 및 초기화 버튼 구현

--- client/js/productList/productList.js
+++ client/js/productList/productList.js
@@ -1,10 +1,23 @@
-import { getNodes } from "../../lib/dom/getNode.js";
+import { addClass, css, removeClass } from "../../lib/dom/css.js";
+import { getNode, getNodes } from "../../lib/dom/getNode.js";
 
 const categoryTitle = getNodes(".top_menu");
 const categoryList = getNodes(".list_wrapper");
 const img = getNodes(".toggle_btn > svg");
 const list = getNodes(".category_list");
 const checked = getNodes(".check_btn");
+const resetBtn = getNode(".reset_btn");
+
+function reset() {
+  resetBtn.style.color = null;
+  //removeClass(".none_btn", "none_btn");
+
+  checked.forEach((checkbox) => {
+    checkbox.classList.remove("none_btn");
+  });
+}
+
+resetBtn.addEventListener("click", reset);
 
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener("click", function (e) {
@@ -13,6 +26,8 @@ for (let i = 0; i < list.length; i++) {
     if (!iconBtn) return;
 
     checked[i].classList.toggle("none_btn");
+
+    css(resetBtn, "color", "#333");
   });
 }
 
