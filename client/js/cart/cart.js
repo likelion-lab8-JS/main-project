037518b51d92b629c04f8b7bf948332212d98f7d
hayoenn2 <dwd1205@naver.com>
2023-02-04 01:06:55 +0900

    [feat] 장바구니 페이지 마크업 리팩토링 및 체크박스 기능 추가

--- client/js/cart/cart.js
+++ client/js/cart/cart.js
@@ -1,13 +1,61 @@
-import {
-  disableElement,
-  invisibleElement,
-  visibleElement,
-} from "../../lib/dom/controlElement.js";
-import { getNode, getNodes } from "../../lib/dom/getNode.js";
+import { getNodes } from "../../lib/dom/getNode.js";
 
 const list = getNodes(".product_list_box");
 const listTitle = getNodes(".list_title");
 const arrowBtn = getNodes(".close_btn > img");
+const subCheck = getNodes("#sub_check");
+const allCheck = getNodes("#all_check");
+
+// 전체 선택
+for (let i = 0; i < allCheck.length; i++) {
+  allCheck[i].addEventListener("click", function () {
+    if (allCheck[i].checked === true) {
+      subCheck.forEach((item) => {
+        item.checked = true;
+      });
+      allCheck[allCheck.length - 1].checked = true;
+    } else {
+      subCheck.forEach((item) => {
+        item.checked = false;
+      });
+      allCheck[allCheck.length - 1].checked = false;
+    }
+  });
+}
+
+// 개별선택
+for (let i = 0; i < subCheck.length; i++) {
+  subCheck[i].addEventListener("click", function () {
+    if (subCheck[i].checked === subCheck.length) {
+      allCheck.forEach((el) => {
+        el.checked = true;
+      });
+    } else {
+      allCheck.forEach((el) => {
+        el.checked = false;
+      });
+    }
+  });
+}
+
+// 전체선택 구현하기
+// for (let i = 0; i < checkBox.length; i++) {
+//   checkBox[i].addEventListener("click", function () {
+//     checkBox[i].classList.add("on");
+
+//     subCheck.forEach((item) => {
+//       item.classList.add("on");
+//     });
+//     checkBox[checkBox.length - 1].classList.add("on");
+//   });
+// }
+
+// // 개별선택 구현하기
+// for (let i = 0; i < subCheck.length; i++) {
+//   subCheck[i].addEventListener("click", function () {
+//     subCheck[i].classList.add("on");
+//   });
+// }
 
 for (let i = 0; i < listTitle.length; i++) {
   listTitle[i].addEventListener("click", function (e) {
