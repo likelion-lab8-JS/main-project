a414feaebbeaf9a3df1e20de09ca3a49ae29a9b1
hayoenn2 <dwd1205@naver.com>
2023-02-04 04:13:20 +0900

    [feat] 장바구니 수량 증가 버튼 기능 구현 #49

--- client/js/cart/cart.js
+++ client/js/cart/cart.js
@@ -1,11 +1,7 @@
-import { getNodes } from "../../lib/dom/getNode.js";
+import { getNode, getNodes } from "../../lib/dom/getNode.js";
 
-const list = getNodes(".product_list_box");
-const listTitle = getNodes(".list_title");
-const arrowBtn = getNodes(".close_btn > img");
 const subCheck = getNodes("#sub_check");
 const allCheck = getNodes("#all_check");
-
 // 체크박스
 // 1. 전체 선택
 for (let i = 0; i < allCheck.length; i++) {
@@ -25,21 +21,41 @@ for (let i = 0; i < allCheck.length; i++) {
 }
 
 // 2. 개별선택
+let count = 0;
+
 for (let i = 0; i < subCheck.length; i++) {
   subCheck[i].addEventListener("click", function () {
-    if (subCheck[i].checked === subCheck.length) {
-      allCheck.forEach((el) => {
-        el.checked = true;
+    if (subCheck[i].checked === true) {
+      count++;
+    }
+
+    if (subCheck.length === count) {
+      allCheck.forEach((item) => {
+        item.checked = true;
       });
     } else {
-      allCheck.forEach((el) => {
-        el.checked = false;
+      allCheck.forEach((item) => {
+        item.checked = false;
       });
     }
+
+    // if (subCheck[i].checked === subCheck.length) {
+    //   allCheck.forEach((el) => {
+    //     el.checked = true;
+    //   });
+    // } else {
+    //   allCheck.forEach((el) => {
+    //     el.checked = false;
+    //   });
+    // }
   });
 }
 
 // 리스트 클릭시 아코디언메뉴 구현
+const list = getNodes(".product_list_box");
+const listTitle = getNodes(".list_title");
+const arrowBtn = getNodes(".close_btn > img");
+
 for (let i = 0; i < listTitle.length; i++) {
   listTitle[i].addEventListener("click", function (e) {
     let closeBtn = e.target.closest(".list_title");
@@ -50,3 +66,30 @@ for (let i = 0; i < listTitle.length; i++) {
     list[i].classList.toggle("active");
   });
 }
+
+// 클릭시 수량 증가
+const plusBtn = getNodes(".plus_btn");
+const minusBtn = getNodes(".minus_btn");
+
+function getPlusCount(e) {
+  let productCount = e.target.previousElementSibling;
+  if (!productCount) return;
+
+  let num = +productCount.innerText;
+
+  if (plusBtn) {
+    num++;
+  }
+
+  productCount.innerText = num;
+
+  if (num !== 1) {
+    const getImgNode = e.target.closest(".plus_btn");
+    getImgNode.classList.add("active");
+  }
+  console.log(num);
+}
+
+plusBtn.forEach((el) => {
+  el.addEventListener("click", getPlusCount);
+});
