03a651bacf15e1b10f49e957ba3702f3da2cfc7b
hayoenn2 <dwd1205@naver.com>
2023-02-06 19:46:56 +0900

    [refac] 상품리스트 페이지 모듈함수를 이용해 기능 리팩토링 #61

--- client/js/productList/productList.js
+++ client/js/productList/productList.js
@@ -1,42 +1,41 @@
-import { addClass, css, removeClass } from "../../lib/dom/css.js";
+import { css, removeClass, toggleClass } from "../../lib/dom/css.js";
 import { getNode, getNodes } from "../../lib/dom/getNode.js";
 
 const categoryTitle = getNodes(".top_menu");
 const categoryList = getNodes(".list_wrapper");
-const img = getNodes(".toggle_btn > svg");
+const buttonImg = getNodes(".toggle_button > img");
 const list = getNodes(".category_list");
-const checked = getNodes(".check_btn");
-const resetBtn = getNode(".reset_btn");
+const checked = getNodes(".check_button");
+const resetButton = getNode(".reset_button");
 
-function reset() {
-  resetBtn.style.color = null;
-  //removeClass(".none_btn", "none_btn");
+function resetCategoryList() {
+  resetButton.style.color = null;
 
   checked.forEach((checkbox) => {
-    checkbox.classList.remove("none_btn");
+    removeClass(checkbox, "none_button");
   });
 }
 
-resetBtn.addEventListener("click", reset);
+resetButton.addEventListener("click", resetCategoryList);
 
 for (let i = 0; i < list.length; i++) {
   list[i].addEventListener("click", function (e) {
     e.preventDefault();
-    let iconBtn = e.target.closest(".category_list > a");
-    if (!iconBtn) return;
+    let iconButton = e.target.closest(".category_list");
+    if (!iconButton) return;
 
-    checked[i].classList.toggle("none_btn");
+    toggleClass(checked[i], "none_button");
 
-    css(resetBtn, "color", "#333");
+    css(resetButton, "color", "#333");
   });
 }
 
 for (let i = 0; i < categoryTitle.length; i++) {
   categoryTitle[i].addEventListener("click", function (e) {
-    let toggleBtn = e.target.closest(".toggle_btn");
-    if (!toggleBtn) return;
+    let toggleButton = e.target.closest(".toggle_button");
+    if (!toggleButton) return;
 
-    img[i].classList.toggle("active");
+    toggleClass(buttonImg[i], "active");
 
     if (categoryList[i].style.maxHeight) {
       categoryList[i].style.maxHeight = null;
@@ -45,3 +44,24 @@ for (let i = 0; i < categoryTitle.length; i++) {
     }
   });
 }
+
+// function toggleCategory(e) {
+//   let toggleButton = e.target.closest(".toggle_button");
+//   if (!toggleButton) return;
+
+//   img.forEach((el) => {
+//     el.classList.toggle("active");
+//   });
+
+//   categoryList.forEach((el) => {
+//     if (el.style.maxHeight) {
+//       el.style.maxHeight = null;
+//     } else {
+//       el.style.maxHeight = el.scrollHeight + "px";
+//     }
+//   });
+// }
+
+// categoryTitle.forEach((el) => {
+//   el.addEventListener("click", toggleCategory);
+// });
